6fb740fc2a99e4d6fbfa96d97961ce445340c6ac
Alena <alittlekindness94@gmail.com>
2025-01-25 13:09:42 +0500

    upd

---
+++ src/application.py
@@ -0,0 +1,35 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from pathlib import Path
+
+hostName = "localhost"
+serverPort = 8080
+
+
+class MyRequests(BaseHTTPRequestHandler):
+    """Класс для обработки входящих запросов"""
+
+    def do_GET(self):
+        """Метод для обработки входящих GET-запросов"""
+        html_dir = Path("C:/Users/Alena/my_1/the_basics_of_layout/html")
+        file_path = html_dir / "contacts_page.html"
+
+        if file_path.exists():
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=utf-8")
+            self.end_headers()
+            with open(file_path, "r", encoding="utf-8") as file:
+                content = file.read()
+            self.wfile.write(content.encode("utf-8"))
+        else:
+            self.send_error(404, "HTML Страница не найдена")
+
+
+def run(server_class=HTTPServer, handler_class=MyRequests, port=serverPort):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Serving on port {port}')
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
